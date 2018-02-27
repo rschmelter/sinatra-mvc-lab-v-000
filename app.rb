0e7b74fbd6102f44fb317c1d9e4b6b579bf5174d
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-27 01:27:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
+
+require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    pl = PigLatinizer.new
+    @piglatinized = pl.to_pig_latin(params[:user_phrase])
+    erb :results
+  end
+
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,28 @@
+class PigLatinizer
+
+  def piglatinize(word)
+
+    if !consonant?(word[0])
+      word = word + "w"
+
+    elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
+      word = word.slice(3..-1) + word.slice(0,3)
+
+    elsif consonant?(word[0]) && consonant?(word[1])
+      word = word.slice(2..-1) + word.slice(0,2)
+
+    else
+      word = word.slice(1..-1) + word.slice(0)
+    end
+    word << "ay"
+  end
+
+  def consonant?(char)
+    !char.match(/[aAeEiIoOuU]/)
+  end
+
+  def to_pig_latin(sentence)
+    sentence.split.collect { |word| piglatinize(word) }.join(" ")
+  end
+
+end
